df9fca03c1390829e7199ac2339ee4abb89a7e41
Ghebr0us <ghebrous.davide@iisgalvanimi.edu.it>
2022-03-08 12:56:59 +0000

    ò

--- Esercizio1/Es2.py
+++ Esercizio1/Es2.py
@@ -7,19 +7,30 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 data = []
-@app.route('/logup', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def log1():
+    if request.method == 'POST':    
+
+        nm = request.args['name']
+        us = request.args['username']
+        pw = request.args['Password']
+        confpw = request.args['ConfPassword']
+        sex = request.form.get('Sex')
+
+    else:
+        if nm == '' or us == '' or pw == '' or confpw == '' or sex == '':
+            print('Errore')
+            if confpw != pw:
+                print('Password diverse')
+            else:
+                data.append({'name': nm, 'username' : us, 'password' : pw, 'conferma_password' : confpw, 'sesso': sex})
+            print(data)
+        return render_template("log.html", nm= nm, us = us, pw = pw , confpw=confpw,data= data)
     return render_template("register.html" )
 
 
-@app.route('/login', methods=['GET'])
+@app.route('/login', methods=['GET','POST'])
 def log2():
-    nm = request.args['name']
-    us = request.args['username']
-    pw = request.args['Password']
-    confpw = request.args['ConfPassword']
-    data.append({name: nm, username : us, password : pw, conferma_password : confpw})
-    return render_template("register.html", nm= nm, us = us, pw = pw , confpw=confpw)
-
+    
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
