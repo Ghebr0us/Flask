a77fe27e4eb99f2c50386f07ffc243f2c5712561
Ghebr0us <ghebrous.davide@iisgalvanimi.edu.it>
2022-03-22 12:02:58 +0000

    SDF

--- EsercizioGrafico/app7_input.py
+++ EsercizioGrafico/app7_input.py
@@ -1,6 +1,6 @@
 # realizzare un sito web che restituisca la mappa dei quartieri di Milano.
 #ci deve essere una home page con un link "quartieri di Milano": cliccando su questo link si deve visualizzare la mappa dei quartieri di Milano
-from flask import Flask, render_template, request,send_file, make_response, url_for, Response
+from flask import Flask, render_template, request,send_file, make_response, url_for, Response 
 app = Flask(__name__)
 import io
 import geopandas as gpd
@@ -16,14 +16,14 @@ quartieri = gpd.read_file('/workspace/Flask/EsercizioGrafico/static/ds964_nil_wm
 @app.route('/', methods = ['GET'])
 def home():
     
-    return render_template('plot.html')
+    return render_template('home.html')
 
 @app.route('/plot.png', methods=['GET'])
 def plot_png():
 
     fig, ax = plt.subplots(figsize = (12,8))
 
-    quartieri.to_crs(epsg=3857).plot(ax=ax, alpha=0.5)
+    imgutente.to_crs(epsg=3857).plot(ax=ax, alpha=0.5)
     contextily.add_basemap(ax=ax)
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
@@ -31,8 +31,10 @@ def plot_png():
 
 @app.route('/plot', methods=("POST", "GET"))
 def mpl():
-    return render_template('plot.html',
-                           PageTitle = "Matplotlib")
+    global imgutente
+    quartiere_input = request.args['Quartiere']
+    imgutente = quartieri[quartieri['NIL'] == quartiere_input]
+    return render_template('plot.html', PageTitle = "Matplotlib")
 
 
 if __name__ == '__main__':
