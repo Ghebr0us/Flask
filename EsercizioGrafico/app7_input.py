2e43fe391b8442ab7b9b457c8d22cc90af6fe125
Ghebr0us <ghebrous.davide@iisgalvanimi.edu.it>
2022-03-23 13:30:08 +0000

    ok

--- EsercizioGrafico/app7_input.py
+++ EsercizioGrafico/app7_input.py
@@ -1,18 +1,3 @@
-# Realizzare un sito web che permetta di visualizzare i quartieri di Milano e alcune informaizoni. La homepage deve contenere i link alle seguenti pagine
-
-# /visualizza 
-# visualizza la mappa di Milano divisa in quartieri
-
-# /ricerca
-# permette all'utente di inserire il nome di un quartiere e di avere la mappa del quartiere inserito. Dare un opportuno messaggio di errore se il quartiere non dovesse esistere
-
-# /scelta
-# permette all'utente di scegliere il nome del quartiere da un menù a tendina contenente i nomi di tutti i quartieri e di visualizzarlo
-
-# /fontanelle
-# permette all'utente di scegliere il nome del quartiere da un menù a tendina contenente i nomi di tutti i quartieri e di visualizzare le fontanelle presenti in quel quartiere. Visualizzare anche l'elenco delle fontanelle con l'indirizzo di ognuna
-
-# Ogni link deve restituire una pagina html contenente un messagigo opportuno e le informazioni richieste.
 
 from flask import Flask, render_template, request,send_file, make_response, url_for, Response 
 app = Flask(__name__)
